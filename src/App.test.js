a8ec6bb8af5b72d78b22676cafc290e837fb9a0e
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-09-03 01:16:35 +0200

    init test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import App from "./App";
+import Enzyme, { shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+Enzyme.configure({ adapter: new Adapter() });
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("render without crashing", () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper).toBeTruthy();
 });
