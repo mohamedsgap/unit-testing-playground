6d522caf6568100380382c43ce2031064ec5e1c6
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-09-06 10:22:31 +0200

    complete app test specs

--- src/ClickCounter.js
+++ src/ClickCounter.js
@@ -7,19 +7,35 @@ class ClickCounter extends Component {
 
     this.state = {
       counter: 0,
+      error: "",
     };
   }
+  incrementCounter = () => {
+    this.setState({ counter: this.state.counter + 1 });
+    if (this.state.counter === 0) {
+      this.setState({ error: "" });
+    }
+  };
+  decrementCounter = () => {
+    this.setState({ counter: this.state.counter - 1 });
+    if (this.state.counter <= 0) {
+      this.setState({ error: "can't go below ZERO" });
+      this.setState({ counter: (this.state.counter = 0) });
+    }
+  };
+
   render() {
     return (
       <div data-test="component-app">
         <h1 data-test="counter-display">
           The counter is currently {this.state.counter}
         </h1>
-        <button
-          data-test="increment-button"
-          onClick={() => this.setState({ counter: this.state.counter + 1 })}
-        >
+        <h3 data-test="error-message">{this.state.error}</h3>
+        <button data-test="increment-button" onClick={this.incrementCounter}>
           Increment counter
+        </button>
+        <button data-test="decrement-button" onClick={this.decrementCounter}>
+          Decrement counter
         </button>
       </div>
     );
--- src/ClickCounter.test.js
+++ src/ClickCounter.test.js
@@ -4,16 +4,84 @@ import Enzyme, { shallow } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 Enzyme.configure({ adapter: new Adapter() });
 
+/**
+ *
+ * @param {props value of ClickCounter} props
+ * @param {the state of ClickCounter} state
+ */
+
+const setup = (props = {}, state = null) => {
+  const wrapper = shallow(<ClickCounter {...props} />);
+  if (state) wrapper.setState(state);
+  return wrapper;
+};
+/**
+ *
+ * @param {Wrapper } wrapper
+ * @param {data-test attribute} val
+ */
+
+const findByAttr = (wrapper, val) => {
+  return wrapper.find(`[data-test="${val}"]`);
+};
+
 test("render without errors", () => {
-  const wrapper = shallow(<ClickCounter />);
-  const appComponent = wrapper.find("[data-test='component-app']");
+  const wrapper = setup();
+  const appComponent = findByAttr(wrapper, "component-app");
   expect(appComponent.length).toEqual(1);
 });
 
-test("renders increamet  button", () => {});
+test("renders increamet  button", () => {
+  const wrapper = setup();
+  const button = findByAttr(wrapper, "increment-button");
+  expect(button.length).toEqual(1);
+});
 
-test("renders counter display", () => {});
+test("renders counter display", () => {
+  const wrapper = setup();
+  const counterDisplay = findByAttr(wrapper, "counter-display");
+  expect(counterDisplay.length).toEqual(1);
+});
 
-test("renders counter  starts 0", () => {});
+test("renders counter  starts 0", () => {
+  const wrapper = setup();
+  const initialCounterState = wrapper.state("counter");
+  expect(initialCounterState).toEqual(0);
+  expect(initialCounterState).toBe(0);
+});
 
-test("renders clicking button increaments counter display", () => {});
+test("renders clicking button increaments counter display", () => {
+  const counter = 13;
+  const wrapper = setup(null, { counter });
+  // find button and click
+  const button = findByAttr(wrapper, "increment-button");
+  button.simulate("click");
+
+  // find display and test value
+  const counterDisplay = findByAttr(wrapper, "counter-display");
+  expect(counterDisplay.text()).toContain(counter + 1);
+});
+
+test("renders decreamet  button", () => {
+  const wrapper = setup();
+  const button = findByAttr(wrapper, "decrement-button");
+  expect(button.length).toEqual(1);
+});
+
+test("renders clicking button decrement counter display", () => {
+  const counter = 13;
+  const wrapper = setup(null, { counter });
+  // find button and click
+  const button = findByAttr(wrapper, "decrement-button");
+  button.simulate("click");
+
+  // find display and test value
+  const counterDisplay = findByAttr(wrapper, "counter-display");
+  expect(counterDisplay.text()).toContain(counter - 1);
+});
+
+test(" render error message on page", () => {
+  const wrapper = setup();
+  const error = findByAttr(wrapper, "error-message");
+  expect(error.length).toEqual(1);
+});
