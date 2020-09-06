5ab77fb8988a2763f6dbb656b74f2e477b5ec393
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-09-06 08:54:29 +0200

    test: renders without error

--- src/ClickCounter.test.js
+++ src/ClickCounter.test.js
@@ -3,3 +3,17 @@ import ClickCounter from "./ClickCounter";
 import Enzyme, { shallow } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 Enzyme.configure({ adapter: new Adapter() });
+
+test("render without errors", () => {
+  const wrapper = shallow(<ClickCounter />);
+  const appComponent = wrapper.find("[data-test='component-app']");
+  expect(appComponent.length).toEqual(1);
+});
+
+test("renders increamet  button", () => {});
+
+test("renders counter display", () => {});
+
+test("renders counter  starts 0", () => {});
+
+test("renders clicking button increaments counter display", () => {});
