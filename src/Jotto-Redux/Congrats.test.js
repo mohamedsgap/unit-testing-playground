c6e05eea579a9245bf678227375170abe2ee2526
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-09-08 18:22:19 +0200

    compelte test specs for Congrats

--- src/Jotto-Redux/Congrats.test.js
+++ src/Jotto-Redux/Congrats.test.js
@@ -15,8 +15,20 @@ const setup = (props = {}) => {
   return shallow(<Congrats {...props} />);
 };
 
-test("renders witout errors", () => {});
+test("renders witout errors", () => {
+  const wrapper = setup();
+  const component = findByTestAttr(wrapper, "congrats");
+  expect(component.length).toBe(1);
+});
 
-test("renders no text with `success` props is false", () => {});
+test("renders no text with `success` props is false", () => {
+  const wrapper = setup({ success: false });
+  const component = findByTestAttr(wrapper, "congrats");
+  expect(component.text()).toBe("");
+});
 
-test("renders non-empty congrats message when `success` props is true", () => {});
+test("renders non-empty congrats message when `success` props is true", () => {
+  const wrapper = setup({ success: true });
+  const message = findByTestAttr(wrapper, "congrats-message");
+  expect(message.text().length).not.toBe(0);
+});
