582f5c1410fd616b28c6d6727cee571682269d7c
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-09-08 17:54:04 +0200

    create hlper test funtions

--- src/Jotto-Redux/Congrats.test.js
+++ src/Jotto-Redux/Congrats.test.js
@@ -1,7 +1,22 @@
 import React from "react";
 import Enzyme, { shallow } from "enzyme";
 import EnzymeAdapter from "enzyme-adapter-react-16";
-
+import { findByTestAttr } from "../../test/testUtils";
 import Congrats from "./Congrats";
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+/**
+ * @funtion setup
+ * @returns(wrapper with props)
+ */
+
+const setup = (props = {}) => {
+  return shallow(<Congrats {...props} />);
+};
+
+test("renders witout errors", () => {});
+
+test("renders no text with `success` props is false", () => {});
+
+test("renders non-empty congrats message when `success` props is true", () => {});
---
+++ test/testUtils.js
@@ -0,0 +1,9 @@
+/**
+ *
+ * @param {shallowWrapper} wrapper
+ * @param {string} val
+ */
+
+export const findByTestAttr = (wrapper, val) => {
+  return wrapper.find(`[data-test=${val}]`);
+};
