a28ee9bb3b0820b35ae1acaa6493f53b5eb90b3d
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-09-08 18:51:10 +0200

    update tests

--- src/Jotto-Redux/Congrats.test.js
+++ src/Jotto-Redux/Congrats.test.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Enzyme, { shallow } from "enzyme";
 import EnzymeAdapter from "enzyme-adapter-react-16";
-import { findByTestAttr } from "../../test/testUtils";
+import { findByTestAttr, checkProps } from "../../test/testUtils";
 import Congrats from "./Congrats";
-
 Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+const defaultProps = { success: false };
 
 /**
  * @funtion setup
@@ -12,7 +13,8 @@ Enzyme.configure({ adapter: new EnzymeAdapter() });
  */
 
 const setup = (props = {}) => {
-  return shallow(<Congrats {...props} />);
+  const setupProps = { ...defaultProps, ...props };
+  return shallow(<Congrats {...setupProps} />);
 };
 
 test("renders witout errors", () => {
@@ -31,4 +33,9 @@ test("renders non-empty congrats message when `success` props is true", () => {
   const wrapper = setup({ success: true });
   const message = findByTestAttr(wrapper, "congrats-message");
   expect(message.text().length).not.toBe(0);
+});
+
+test("does not throw wraning with expected props", () => {
+  const expectedProps = { success: false };
+  checkProps(Congrats, expectedProps);
 });
--- test/testUtils.js
+++ test/testUtils.js
@@ -1,3 +1,4 @@
+import checkPropTypes from "check-prop-types";
 /**
  *
  * @param {shallowWrapper} wrapper
@@ -6,4 +7,14 @@
 
 export const findByTestAttr = (wrapper, val) => {
   return wrapper.find(`[data-test="${val}"]`);
+};
+
+export const checkProps = (component, confirmingProps) => {
+  const propError = checkPropTypes(
+    component.propTypes,
+    confirmingProps,
+    "prop",
+    component.name
+  );
+  expect(propError).toBeUndefined();
 };
