08367215ea1bc92f430bdbeb23939f3e816aa25e
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-09-08 18:22:44 +0200

    built a Congrats component

--- src/Jotto-Redux/Congrats.js
+++ src/Jotto-Redux/Congrats.js
@@ -1,16 +1,28 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 /**
- * Functional react component for congrats message
- * @funtion
- * @return {JSX.Element} Rendered component (or null if success props is passed)
+ * Functional react component for congratulatory message.
+ * @function
+ * @param {object} props - React props.
+ * @returns {JSX.Element} - Rendered component (or null if `success` prop is false).
  */
 const Congrats = (props) => {
-  return (
-    <div>
-      <p>jfoidjaf</p>
-    </div>
-  );
+  if (props.success) {
+    return (
+      <div data-test="congrats" className="alert alert-success">
+        <span data-test="congrats-message">
+          Congratulations! You guessed the word!
+        </span>
+      </div>
+    );
+  } else {
+    return <div data-test="congrats" />;
+  }
+};
+
+Congrats.propTypes = {
+  success: PropTypes.bool.isRequired,
 };
 
 export default Congrats;
