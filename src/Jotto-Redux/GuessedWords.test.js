8d71db3332cb391f92c94b32a998a32a109e2274
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-09-10 18:32:14 +0200

    write test spces for no guessed words

--- src/Jotto-Redux/GuessedWords.test.js
+++ src/Jotto-Redux/GuessedWords.test.js
@@ -18,7 +18,18 @@ test("doesn't throw warning with expected props", () => {
 });
 
 describe("if there are no guessed words", ()=> {
-
+    let wrapper;
+    beforeEach(()=> {
+        wrapper = setup({GuessedWords: []})
+    })
+    test("render without error", () => {
+        const component = findByTestAttr(wrapper, "component-guessed-words");
+        expect(component.length).toBe(1)
+    })
+    test("render instructions to guess a word", () => {
+        const instructions = findByTestAttr(wrapper, "guess-instructions");
+        expect(instructions.text().length).not.toBe(0)
+    })
 })
 
 describe("if there are guessed words", ()=> {
