fcddf9ed09812738348019de2d54612d1ff16d52
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-09-10 18:59:03 +0200

    complete GuessedWords testSpecs

--- src/Jotto-Redux/GuessedWords.test.js
+++ src/Jotto-Redux/GuessedWords.test.js
@@ -33,5 +33,28 @@ describe("if there are no guessed words", ()=> {
 })
 
 describe("if there are guessed words", ()=> {
+    let wrapper;
+    const guessedWords = [
+        {guessedWord: 'train', letterMatchCount: 3},
+        {guessedWord: 'agile', letterMatchCount: 1}, 
+        {guessedWord: 'party', letterMatchCount: 5}, 
+    ]
+    beforeEach(()=> {
+        wrapper = setup({GuessedWords})
+    })
+    test("render without error", () => {
+        const component = findByTestAttr(wrapper, "component-guessed-words");
+        expect(component.length).toBe(1)
+
+    })
+    test("render guessed words section", () => {
+        const guessedWordsNode = findByTestAttr(wrapper, "guessed-words");
+        expect(guessedWordsNode.length).toBe(1)
+
+    })
+    test("corrent number of guessed words", () => {
+        const guessedWordsNodes = findByTestAttr(wrapper, "guessed-word");
+        expect(guessedWordsNodes.length).toBe(guessedWords.length)
+    })
 
 })
