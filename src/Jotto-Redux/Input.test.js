b690e0c152b607dbeb9ff4be286bfa43dbbfa091
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-09-27 00:38:19 +0200

    complete test specs for Input component

--- src/Jotto-Redux/Input.test.js
+++ src/Jotto-Redux/Input.test.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { shallow } from "enzyme";
-import {
-  findByTestAttr,
-  storeFactory,
-} from "../../test/testUtils";
+
+import { findByTestAttr, storeFactory } from "../test/testUtils";
 import Input, { UnconnectedInput } from "./Input";
 
+/**
+ * Factory function to create a ShallowWrapper for the Input component.
+ * @function setup
+ * @param {object} initialState - Initial state for this setup.
+ * @returns {ShallowWrapper}
+ */
 const setup = (initialState = {}) => {
   const store = storeFactory(initialState);
   const wrapper = shallow(<Input store={store} />)
@@ -14,22 +18,22 @@ const setup = (initialState = {}) => {
   return wrapper;
 };
 
-describe(" render ", () => {
+describe("render", () => {
   describe("word has not been guessed", () => {
     let wrapper;
     beforeEach(() => {
       const initialState = { success: false };
       wrapper = setup(initialState);
     });
-    test(" renders component without error", () => {
+    test("renders component without error", () => {
       const component = findByTestAttr(wrapper, "component-input");
       expect(component.length).toBe(1);
     });
-    test(" renders the  input box", () => {
+    test("renders input box", () => {
       const inputBox = findByTestAttr(wrapper, "input-box");
       expect(inputBox.length).toBe(1);
     });
-    test(" renders submit  button", () => {
+    test("renders submit button", () => {
       const submitButton = findByTestAttr(wrapper, "submit-button");
       expect(submitButton.length).toBe(1);
     });
@@ -40,19 +44,62 @@ describe(" render ", () => {
       const initialState = { success: true };
       wrapper = setup(initialState);
     });
-    test(" renders component without error", () => {
+    test("renders component without error", () => {
       const component = findByTestAttr(wrapper, "component-input");
       expect(component.length).toBe(1);
     });
-    test(" not renders the  input box", () => {
+    test("does not render input box", () => {
       const inputBox = findByTestAttr(wrapper, "input-box");
       expect(inputBox.length).toBe(0);
     });
-    test(" not renders submit  button", () => {
-      const submitButton = findByTestAttr(wrapper, "submit-button");
-      expect(submitButton.length).toBe(0);
+    test("does not render submit button", () => {
+      const submit = findByTestAttr(wrapper, "submit-button");
+      expect(submit.length).toBe(0);
     });
   });
 });
 
-describe("update state", () => {});
+describe("redux props", () => {
+  test("has success piece of state as prop", () => {
+    const success = true;
+    const wrapper = setup({ success });
+    const successProp = wrapper.instance().props.success;
+    expect(successProp).toBe(success);
+  });
+  test("`guessWord` action creator is a function prop", () => {
+    const wrapper = setup();
+    const guessWordProp = wrapper.instance().props.guessWord;
+    expect(guessWordProp).toBeInstanceOf(Function);
+  });
+});
+
+describe("`guessWord` action creator", () => {
+  let guessWordMock;
+  let wrapper;
+  const guessedWord = "train";
+  beforeEach(() => {
+    // create a mock function for `getSecretWord`
+    guessWordMock = jest.fn();
+
+    // set up Input, with guessWordMock as a prop
+    wrapper = shallow(<UnconnectedInput guessWord={guessWordMock} />);
+
+    // add value to input box
+    wrapper.setState({ currentGuess: guessedWord });
+
+    // simulate click on submit button
+    const submit = findByTestAttr(wrapper, "submit-button");
+    submit.simulate("click", { preventDefault() {} });
+  });
+  test("`guessWord` was called once", () => {
+    const guessWordCallCount = guessWordMock.mock.calls.length;
+    expect(guessWordCallCount).toBe(1);
+  });
+  test("calls `guessWord with input value as argument`", () => {
+    const guessWordArg = guessWordMock.mock.calls[0][0];
+    expect(guessWordArg).toBe(guessedWord);
+  });
+  test("input box clears on submit", () => {
+    expect(wrapper.state("currentGuess")).toBe("");
+  });
+});
