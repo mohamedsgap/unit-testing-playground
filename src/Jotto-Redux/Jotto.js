68103dc7951a51e6f8d77cb1125fcbb53432509d
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-09-27 00:45:45 +0200

    complete Jotto-redux

--- src/Jotto-Redux/Jotto.js
+++ src/Jotto-Redux/Jotto.js
@@ -1,22 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
+import { connect } from "react-redux";
 import "../App.css";
-import Congrats from "./Congrats";
+
 import GuessedWords from "./GuessedWords";
-function Jotto() {
-  return (
-    <div className="App">
-      <h3>Hello, World, It's The Jotto App :) </h3>
-      <Congrats success={true} />
-      <GuessedWords
-        guessedWords={[
-          {
-            guessedWord: "train",
-            letterMatchCount: 3,
-          },
-        ]}
-      />
-    </div>
-  );
+import Congrats from "./Congrats";
+import Input from "./Input";
+import { getSecretWord } from "./actions";
+
+export class Jotto extends Component {
+  /**
+   * @method componentDidMount
+   * @returns {undefined}
+   */
+  componentDidMount() {
+    // get the secret word
+    this.props.getSecretWord();
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <h1>Jotto</h1>
+        <Congrats success={this.props.success} />
+        <Input />
+        <GuessedWords guessedWords={this.props.guessedWords} />
+      </div>
+    );
+  }
 }
 
-export default Jotto;
+const mapStateToProps = (state) => {
+  const { success, guessedWords, secretWord } = state;
+  return { success, guessedWords, secretWord };
+};
+
+export default connect(mapStateToProps, { getSecretWord })(Jotto);
