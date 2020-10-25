fea7b05e23e702af389b2d8fe30d4f99adb7d1c8
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-10-25 21:04:21 +0200

    Add Input component with test specs

--- jotto-context/src/Input.js
+++ jotto-context/src/Input.js
@@ -1,7 +1,34 @@
+import React from "react";
 import propTypes from "prop-types";
 
 const Input = () => {
-  return <div data-test="input-component"></div>;
+  const [currentGuess, setCurrentGuess] = React.useState("");
+
+  const submitValue = (e) => {
+    e.preventDefault();
+    setCurrentGuess("");
+  };
+  return (
+    <div data-test="input-component">
+      <form className="form-inline">
+        <input
+          value={currentGuess}
+          onChange={(e) => setCurrentGuess(e.target.value)}
+          data-test="input-box"
+          className="mb-2 mx-sm-3"
+          type="text"
+          placeholder="enter guess"
+        />
+        <button
+          data-test="submit-button"
+          className="btn btn-primary mb-2"
+          onClick={submitValue}
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Input;
--- jotto-context/src/input.test.js
+++ jotto-context/src/input.test.js
@@ -1,5 +1,5 @@
 import { shallow } from "enzyme";
-import { func } from "prop-types";
+import React from "react";
 import { findByTestAttr, checkProps } from "../test/testUtils";
 import Input from "./Input";
 
@@ -20,4 +20,26 @@ test("Render Input compnent without error", () => {
 
 test("Check Input component props type", () => {
   checkProps(Input, { secretWord: "party" });
+});
+
+describe("state contolled of Input component", () => {
+  const mockSetCurrentGuess = jest.fn();
+  let wrapper;
+  beforeEach(() => {
+    mockSetCurrentGuess.mockClear();
+    React.useState = jest.fn(() => ["", mockSetCurrentGuess]);
+    wrapper = setup();
+  });
+  test("state update of value upon input box", () => {
+    const inputBox = findByTestAttr(wrapper, "input-box");
+    const mockEvent = { target: { value: "train" } };
+    inputBox.simulate("change", mockEvent);
+    expect(mockSetCurrentGuess).toHaveBeenCalledWith("train");
+  });
+
+  test("field is cleared when submit button clicked", () => {
+    const submitButton = findByTestAttr(wrapper, "submit-button");
+    submitButton.simulate("click", { preventDefault() {} });
+    expect(mockSetCurrentGuess).toHaveBeenCalledWith("");
+  });
 });
