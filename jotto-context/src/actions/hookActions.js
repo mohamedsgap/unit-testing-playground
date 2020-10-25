cb83d51b5d2202b4c62a2fa21c6ddeaba5c118a5
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-10-25 21:03:46 +0200

    Make axios call and test it

---
+++ jotto-context/src/actions/hookActions.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+export const getSecretWord = async (setSecretWord) => {
+  const response = await axios.get("http://localhost:3030");
+  setSecretWord(response.data);
+};
+
+// export default for moking convinence
+export default {
+  getSecretWord,
+};
---
+++ jotto-context/src/actions/hookActions.test.js
@@ -0,0 +1,29 @@
+import moxios from "moxios";
+import { getSecretWord } from "./hookActions";
+
+describe("Moxios tests", () => {
+  beforeEach(() => {
+    moxios.install();
+  });
+  afterEach(() => {
+    moxios.uninstall();
+  });
+  test(" call getSecretWord callback on axios reponse", async () => {
+    let secretWord = "party";
+    moxios.wait(() => {
+      const request = moxios.requests.mostRecent();
+      request.respondWith({
+        status: 200,
+        response: secretWord,
+      });
+    });
+
+    // mock on callback arg
+    const mocksetSecretWord = jest.fn();
+
+    await getSecretWord(mocksetSecretWord);
+
+    // see weather mock run with correct arguement
+    expect(mocksetSecretWord).toHaveBeenCalledWith(secretWord);
+  });
+});
