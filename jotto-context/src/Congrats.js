dfc0147f86ca1bf4c0faf3b440af020c4fbee932
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-10-29 10:18:08 +0200

    updte Congrats component using context

--- jotto-context/src/Congrats.js
+++ jotto-context/src/Congrats.js
@@ -1,28 +1,27 @@
 import React from "react";
-import PropTypes from "prop-types";
+import successContext from "./contexts/successContext";
+import languageContext from "./contexts/languageContext";
+import stringsModule from "./helpers/strings";
 
 /**
  * Functional react component for congratulatory message.
  * @function
- * @param {object} props - React props.
  * @returns {JSX.Element} - Rendered component (or null if `success` prop is false).
  */
-const Congrats = (props) => {
-  if (props.success) {
+const Congrats = () => {
+  const [success] = successContext.useSucess();
+  const [language] = React.useContext(languageContext);
+  if (success) {
     return (
       <div data-test="congrats" className="alert alert-success">
         <span data-test="congrats-message">
-          Congratulations! You guessed the word!
+          {stringsModule.getStringByLanguage(language, "congrats")}
         </span>
       </div>
     );
   } else {
     return <div data-test="congrats" />;
   }
-};
-
-Congrats.propTypes = {
-  success: PropTypes.bool.isRequired,
 };
 
 export default Congrats;
