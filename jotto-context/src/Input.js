98ddad1411baa0c19c209db28cec3e18766eb407
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-10-29 10:52:21 +0200

    Update Input with context and TestSpecs

--- jotto-context/src/Input.js
+++ jotto-context/src/Input.js
@@ -1,38 +1,69 @@
 import React from "react";
-import propTypes from "prop-types";
+import PropTypes from "prop-types";
 
-const Input = () => {
+import guessedWordsContext from "./contexts/guessedWordsContext";
+import successContext from "./contexts/successContext";
+import languageContext from "./contexts/languageContext";
+import stringsModule from "./helper/strings";
+import { getLetterMatchCount } from "./helper";
+
+function Input({ secretWord }) {
+  const language = React.useContext(languageContext);
+  const [success, setSuccess] = successContext.useSuccess();
+  const [guessedWords, setGuessedWords] = guessedWordsContext.useGuessedWords();
   const [currentGuess, setCurrentGuess] = React.useState("");
 
-  const submitValue = (e) => {
-    e.preventDefault();
-    setCurrentGuess("");
-  };
+  if (success) {
+    return null;
+  }
+
   return (
-    <div data-test="input-component">
+    <div data-test="component-input">
       <form className="form-inline">
         <input
-          value={currentGuess}
-          onChange={(e) => setCurrentGuess(e.target.value)}
           data-test="input-box"
           className="mb-2 mx-sm-3"
           type="text"
-          placeholder="enter guess"
+          placeholder={stringsModule.getStringByLanguage(
+            language,
+            "guessInputPlaceholder"
+          )}
+          value={currentGuess}
+          onChange={(event) => setCurrentGuess(event.target.value)}
         />
         <button
           data-test="submit-button"
+          onClick={(evt) => {
+            evt.preventDefault();
+            // update guessedWords
+            const letterMatchCount = getLetterMatchCount(
+              currentGuess,
+              secretWord
+            );
+            const newGuessedWords = [
+              ...guessedWords,
+              { guessedWord: currentGuess, letterMatchCount },
+            ];
+            setGuessedWords(newGuessedWords);
+
+            // check against secretWord and update success if needed
+            if (currentGuess === secretWord) {
+              setSuccess(true);
+            }
+            // clear input box
+            setCurrentGuess("");
+          }}
           className="btn btn-primary mb-2"
-          onClick={submitValue}
         >
-          Submit
+          {stringsModule.getStringByLanguage(language, "submit")}
         </button>
       </form>
     </div>
   );
+}
+
+Input.propTypes = {
+  secretWord: PropTypes.string.isRequired,
 };
 
 export default Input;
-
-Input.propTypes = {
-  secretWord: propTypes.string.isRequired,
-};
